7e9c3a08e7221c8b96e2d28b7ec719e578b578c0
Seph-23 <seongmlyu95@gmail.com>
2023-03-30 18:49:59 +0900

    build : 프로젝트를 생성한다.

---
+++ mymvc/src/test/java/project/mymvc/MymvcApplicationTests.java
@@ -0,0 +1,13 @@
+package project.mymvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
